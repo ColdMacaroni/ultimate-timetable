6aa85780dddf8f23529b7aee3e4fb7f56e139e68
Diego Meza <Diego.Meza@student.onslow.school.nz>
2022-06-29 10:43:17 +1200

    getters and setters for spell and attendance code enum

--- model.py
+++ model.py
@@ -2,6 +2,14 @@ from dataclasses import dataclass
 from enum import Enum, auto
 
 
+class AttendanceCode(Enum):
+    PRESENT = auto()
+    LATE = auto()
+    ABSENT = auto()
+    MEDICAL = auto()
+    CANCELLED = auto()
+
+
 class Days(Enum):
     MONDAY = auto()
     TUESDAY = auto()
@@ -27,11 +35,67 @@ class Days(Enum):
 class Spell:
     _class_code: str
     _class_name: str
+
+    # Room is a string because of rooms like T1, T2, gym, etc.
     _class_room: str
+
     _teacher_code: str
     _teacher_name: str
-    _start: tuple[int, int] | list[int, int]
-    _end: tuple[int, int] | list[int, int]
+
+    @property
+    def class_code(self) -> str:
+        return self._class_code
+
+    @class_code.setter
+    def class_code(self, new) -> str:
+        if not isinstance(new, str):
+            raise ValueError("Class code must be a string")
+
+        self._class_code = new
+
+    @property
+    def class_name(self) -> str:
+        return self._class_name
+
+    @class_name.setter
+    def class_name(self, new) -> str:
+        if not isinstance(new, str):
+            raise ValueError("Class name must be a string")
+
+        self._class_name = new
+
+    @property
+    def class_room(self) -> str:
+        return self._class_room
+
+    @class_code.setter
+    def class_code(self, new) -> str:
+        if not isinstance(new, str):
+            raise ValueError("Class code must be a string")
+
+        self._class_code = new
+
+    @property
+    def teacher_code(self) -> str:
+        return self._teacher_code
+
+    @teacher_code.setter
+    def teacher_code(self, new) -> str:
+        if not isinstance(new, str):
+            raise ValueError("Teacher code must be a string")
+
+        self._teacher_code = new
+
+    @property
+    def teacher_name(self) -> str:
+        return self._teacher_name
+
+    @teacher_name.setter
+    def teacher_name(self, new) -> str:
+        if not isinstance(new, str):
+            raise ValueError("Teacher name must be a string")
+
+        self._teacher_name = new
 
 
 class DaySpells:
