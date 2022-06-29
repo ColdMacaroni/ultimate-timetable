55e8979726e090ab7eece5b61e8ed33ce2843f3c
muffin <diegoxe7117@gmail.com>
2022-06-29 19:43:53 +1200

    Create spell objects

--- controller.py
+++ controller.py
@@ -1,5 +1,7 @@
 import view
+import model
 import json
+
 
 class TimetableController:
     def __init__(self, timetable_window: view.TimetableMain,
@@ -7,6 +9,26 @@ class TimetableController:
         self.timetable_window = timetable_window
         self.spell_dict = spell_dict
         self.times_dict = times_dict
+
+        self.spells = self.create_spells(self.spell_dict["spells"])
+        # TODO: Create spellslot object for each spell
+        # TODO: Create DaySpells classes for each day
+
+    def create_spells(self, spells_dict) -> dict[str, model.Spell]:
+        spells = dict()
+
+        for key, val in spells_dict.items():
+            if val is not None:
+                spell = model.Spell(**val)
+
+            # Nones are free spells, they are None and not just another spell
+            # because two types(keys) of spells can be free.
+            else:
+                spell = model.Spell("Free Spell", "", "", "", "")
+
+            spells[key] = spell
+
+        return spells
 
     def run(self):
         self.timetable_window.show()
