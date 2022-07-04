b3331bf2f09bab05dec5df781d9f8823c4a3ca8e
soup <squishysparrow7.7@gmail.com>
2022-07-04 12:27:15 +1200

    use tabwidget instead of dropdown

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
 from model import SpellSlot, Day
 
 app = QtWidgets.QApplication()
@@ -32,7 +32,9 @@ class TimetableMain(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)
 
         # For picking days
-        self.day_combobox = QtWidgets.QComboBox(self)
+        # self.day_combobox = QtWidgets.QComboBox(self)
+        self.days_tabwidget = QtWidgets.QTabWidget(self)
+        self.day_widgets = {str(day): QtWidgets.QWidget(self.days_tabwidget) for day in Day}
 
         # For setting if spell 5 exists
         self.spell5_checkbox = QtWidgets.QCheckBox("Spell 5", self)
@@ -41,22 +43,27 @@ class TimetableMain(QtWidgets.QMainWindow):
 
     def initUI(self):
         self.setCentralWidget(QtWidgets.QWidget())
+        self.setGeometry(QtCore.QRect(100, 100, 400, 500))
 
-        # populate dropdown
-        self.day_combobox.addItems([str(day) for day in Day])
         # TODO: self.day_combobox.curentRowChanged.connect()
+        # Populate tabwidget
+        for name, widg in self.day_widgets.items():
+            self.days_tabwidget.addTab(widg, name)
+        
+        # Put the tabs on the left so we dont need to scroll
+        self.days_tabwidget.setTabPosition(QtWidgets.QTabWidget.TabPosition.West)
 
         # This is the main layout for the window
-        vbox = QtWidgets.QVBoxLayout(self.centralWidget())
+        hbox = QtWidgets.QHBoxLayout(self.centralWidget())
 
         # This will put the combo box and spell 5 checkbox next to each other
-        header_hbox = QtWidgets.QHBoxLayout(self.centralWidget())
+        left_vbox = QtWidgets.QVBoxLayout(self.centralWidget())
 
         # The stretch is because they look weird when right next to each other
-        header_hbox.addWidget(self.day_combobox)
-        header_hbox.addStretch()
-        header_hbox.addWidget(self.spell5_checkbox)
+        # header_hbox.addWidget(self.day_combobox)
+        left_vbox.addWidget(self.spell5_checkbox)
+        left_vbox.addWidget(self.days_tabwidget)
 
-        vbox.addLayout(header_hbox)
+        hbox.addLayout(left_vbox)
 
-        self.centralWidget().setLayout(vbox)
+        self.centralWidget().setLayout(hbox)
