c7314050b965a9bc329faa392357cc405fc00ca3
muffin <diegoxe7117@gmail.com>
2022-07-14 19:10:50 +1200

    valid tests for spell slot and check time str

--- tests/test_model.py
+++ tests/test_model.py
@@ -47,6 +47,15 @@ def test_attendance_str():
         "Wrong string from attendance code"
 
 
+def test_time_str():
+    """
+    Checks that the time is correctly converted to a string
+    """
+    t = Time(7, 5)
+
+    assert str(t) == "07:05", "Incorrect string from Time object"
+
+
 def test_time_hour_setter_valid():
     """
     Check that time dataclass accepts a normal hour
@@ -637,12 +646,12 @@ def test_spell_teacher_name_setter_invalid():
 
 def test_spell_teacher_code_setter_invalid():
     """
-    Check that the teacher_code attr can be changed
-    There is no set format for teacher code.
+    Check that the teacher_code attr can't be set to an invalid type
+    There is no set format for teacher code. Only has to be str
     """
     spell = Spell("Cool name", "CODE", "2", "TEA", "Teacher Name")
 
-    TO_SET = 34.2
+    TO_SET = 0
 
     excepted = False
     try:
@@ -656,3 +665,96 @@ def test_spell_teacher_code_setter_invalid():
 
     assert spell.teacher_code != TO_SET,\
         "Teacher code set despite being invalid"
+
+
+##############
+# Spell Slot #
+##############
+
+##
+# Valid
+def test_spellslot_valid():
+    """
+    Checks that the SpellSlot is created correctly, with all its attributes set
+    and stuff
+    """
+    # These are constants so we can be sure the objects arent modified when put
+    # into the spell slot
+
+    # Spell
+    SPELL_NAME = "Spell Name"
+    SPELL_CODE = "CODE"
+    SPELL_ROOM = "1"
+    SPELL_TEACHER_CODE = "TEA"
+    SPELL_TEACHER_NAME = "Teacher Name"
+
+    spell = Spell(
+        SPELL_NAME,
+        SPELL_CODE,
+        SPELL_ROOM,
+        SPELL_TEACHER_CODE,
+        SPELL_TEACHER_NAME
+    )
+
+    # Start
+    START_HOUR = 4
+    START_MINUTE = 34
+
+    start = Time(START_HOUR, START_MINUTE)
+
+    # End
+    END_HOUR = 5
+    END_MINUTE = 54
+    end = Time(END_HOUR, END_MINUTE)
+
+    ATTENDANCE_CODE = AttendanceCode.PRESENT
+
+    spell_slot = SpellSlot(spell, start, end, ATTENDANCE_CODE)
+
+    # Check spell related stuff
+    assert spell_slot.spell is spell,\
+        "spell attribute set to a different object"
+
+    assert spell_slot.spell.class_name == SPELL_NAME,\
+        "Spell name changed"
+
+    assert spell_slot.spell.class_code == SPELL_CODE,\
+        "Spell code changed"
+
+    assert spell_slot.spell.class_room == SPELL_ROOM,\
+        "Spell room changed"
+
+    assert spell_slot.spell.teacher_code == SPELL_TEACHER_CODE,\
+        "Spell teacher name changed"
+
+    assert spell_slot.spell.teacher_name == SPELL_TEACHER_NAME,\
+        "Spell teacher code changed"
+
+    # Check time related stuff
+    assert spell_slot.start is start,\
+        "Start time object is different"
+
+    assert spell_slot.start.hour == START_HOUR,\
+        "Start hour is different"
+
+    assert spell_slot.start.minute == START_MINUTE,\
+        "Start minute is different"
+
+    assert spell_slot.end is end,\
+        "End time object is different"
+
+    assert spell_slot.end.hour == END_HOUR,\
+        "End hour is different"
+
+    assert spell_slot.end.minute == END_MINUTE,\
+        "End minute is different"
+
+    # Attendance
+    assert spell_slot.attendance is ATTENDANCE_CODE,\
+        "Attendance code set to a different object"
+
+
+def test_spellslot_spell_setter():
+    spell = Spell("Spell name", "CODE", "23", "TEA", "Teacher Name")
+    start = Time(1, 14)
+    end = Time(2, 5)
